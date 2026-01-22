47d7a2bd0265abaf2a483fa488326dba8136dd4d
Matthew Fehr <mattfehr2004@gmail.com>
2026-01-21 22:28:09 -0800

    try to fix email contacct with ports

--- contact-form-server/server.js
+++ contact-form-server/server.js
@@ -51,4 +51,6 @@ app.post('/contact', async (req, res) => {
 });
 
 //start the server on localhost:3000 for local deployment
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
